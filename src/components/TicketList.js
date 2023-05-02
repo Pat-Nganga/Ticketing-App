5b2e89ff8b2792705ab3ed6b459a3ab31f14ad06
johnson <johnsonnjichi@gmail.com>
2023-05-02 22:06:19 +0300

    added strong tag

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -12,27 +12,31 @@ function TicketList() {
   }, [])
 
   return (
-    <div className='ticket-list-container'>
+    <div className="ticket-list-container">
       {/* <h1>Ticket List</h1> */}
-      <div className='ticket-cards-container'>
+      <div className="ticket-cards-container">
         {tickets.map((ticket) => (
-          <div key={ticket.id} className='ticket-card'>
+          <div key={ticket.id} className="ticket-card">
             <img
               src={ticket.image}
               alt={ticket.name}
-              className='ticket-image'
+              className="ticket-image"
             />
-            <div className='ticket-card'>
-              <h2 className='ticket-name'>{ticket.name}</h2>
-              <p className='ticket-location'>Location:{ticket.location}</p>
-              <p className='ticket-date'>Date:{ticket.date}</p>
-              <p className='ticket-capacity'>Remaining tickets:::{ticket.capacity}</p>
+            <div className="ticket-card">
+              <h2 className="ticket-name">{ticket.name}</h2>
+              <p className="ticket-location">
+                <strong>Location:</strong>{ticket.location}
+              </p>
+              <p className="ticket-date"><strong>Date:</strong>{ticket.date}</p>
+              <p className="ticket-capacity">
+               <strong> Remaining tickets::</strong>{ticket.capacity}
+              </p>
             </div>
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default TicketList
