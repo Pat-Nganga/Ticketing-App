7dbc90b3cfd9169f66c17b6698d16bb6e8a0160f
Patricia <wambuipatricia3@gmail.com>
2023-05-02 11:57:32 +0300

    Add EventList.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+// import EventList from './EventList';
+
+function App() {
+  return 
+    
+    // <EventList />
+  
+}
+
+export default App;
---
+++ src/components/EventList.js
@@ -0,0 +1,23 @@
+// import React, {useState, useEffect} from 'react'
+
+// function EventList() {
+//  const [eventList, setEventList]= useState([])
+
+
+// useEffect(() => fetch('http://localhost:4300/tickets'))
+// .then((response => response.json())
+// .then((eventList)=>setEventList(eventList))
+
+
+
+// )
+
+
+
+
+//   return (
+    
+//   )
+// }
+
+// export default EventList
