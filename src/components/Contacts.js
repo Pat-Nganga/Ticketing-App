f5934f7f3ac4be102c40683a8f3cccd4d0e2fd2f
Patricia <wambuipatricia3@gmail.com>
2023-05-04 13:49:30 +0300

    Modify code

---
+++ src/components/Contacts.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+// const Contacts = () => {
+//   return (
+//     <div>
+//       <h2>Contact Us</h2>
+//       <p>For any inquiries, please contact us at:</p>
+//       <ul>
+//         <li>Email: info@ticktopia.com</li>
+//         <li>Phone: +1 (123) 456-7890</li>
+//         <li>Address: 123 Main St, Anytown, USA</li>
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default Contacts
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,83 +1,21 @@
-// import React, { useState, useEffect } from 'react'
-//  import './css/NavBar.css'
 
-// function NavBar() {
-//   const [searchTerm, setSearchTerm] = useState('')
-//   const [tickets, setTickets] = useState([])
-
-//   useEffect(() => {
-//     fetch('http://localhost:4300/tickets')
-//       .then((response) => response.json())
-//       .then((tickets) => setTickets(tickets))
-//   }, [])
-
-//   const handleSearchSubmit = (event) => {
-//     setSearchTerm(event.target.value)
-//   }
-
-//   // const filteredTickets = tickets.filter((ticket) =>
-//   //   ticket.name.toLowerCase().includes(searchTerm.toLowerCase())
-//   //)
-
-//   return (
-//     <div className='menu'>
-//       <div className='logo'>
-//         <h3>TICKETOPIA</h3>
-//       </div>
-//       <nav>
-//         <ul className='navigation'>
-//           <li className='nav-link'>
-//             <a href='/'>Home</a>
-//           </li>
-//           <li className='nav-link'>
-//             <a href='/contacts'>Contacts</a>
-//           </li>
-//           <li className='nav-link'>
-//             <a href='/events'>Events</a>
-//           </li>
-//         </ul>
-//       </nav>
-//       <div className='search'>
-//         <form>
-//           <input
-//             type='text'
-//             placeholder='Search here'
-//             value={searchTerm}
-//             // onChange={(e) => handleSearchSubmit(e.target.value)}
-//             id='search-input'
-//             className='input'
-//           />
-//           <button
-//             type='submit'
-//             onSubmit={(e) => handleSearchSubmit(e.target.value)}
-//             className='search-button'
-//           >
-//             Search
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default NavBar
 
 import React, { useState } from 'react'
 import './css/NavBar.css'
 
-const NavBar = ({ setData, fetchData, setEndPoint }) => {
+const NavBar = ({ setData, fetchData, setEndPoint,searchEvent }) => {
   const [searchTerm, setSearchTerm] = useState('')
 
   const handleSearchSubmit = (text) => {
     setSearchTerm(text)
 
     const url = `http://localhost:4300/tickets?_sort=id&_order=desc&q=${text}`
-    // console.log('text', text)
+     console.log('text', text)
     // console.log('url', url)
     setEndPoint(url)
     fetchData()
       .then((res) => {
-        // console.log('res>>', res)
+        console.log('res>>', res)
         setData(res)
       })
       .catch((error) => {
@@ -88,7 +26,7 @@ const NavBar = ({ setData, fetchData, setEndPoint }) => {
   const handleSubmit = (event) => {
     event.preventDefault()
     const text = event.target.elements[0].value
-    handleSearchSubmit(text)
+    searchEvent(text)
   }
 
   return (
