a3af00e40c7f41da39f312746f3e224209a7bf74
johnson <johnsonnjichi@gmail.com>
2023-05-04 14:54:55 +0300

    commented out code

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -4,54 +4,6 @@ import "./css/TicketList.css";
 function TicketList({tickets,ticketId, updateCapacity}) {
  tickets.map((ticket)=>{console.log(ticket.id);})
 
- 
-
-
-  function handleUpdateTicket(updatedTicket) {
-    const updatedTickets = tickets.map((ticket) =>
-      ticket.id === updatedTicket.id ? updatedTicket : ticket
-    );
-    setTickets(updatedTickets);
-
-    fetch(`http://localhost:4300/tickets/${updatedTicket.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedTicket),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
-  }
-
-  function updateCapacity(ticketId) {
-    const updatedTickets = tickets.map((ticket) => {
-      if (ticket.id === ticketId) {
-        return {
-          ...ticket,
-          available_tickets: ticket.available_tickets - 1,
-        };
-      }
-      return ticket;
-    });
-    setTickets(updatedTickets);
-  }
-
-  function handleDelete(ticketId) {
-    const updatedTickets = tickets.filter((ticket) => ticket.id !== ticketId);
-    setTickets(updatedTickets);
-
-    fetch(`http://localhost:4300/tickets/${ticketId}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
-  }
 
   return (
     <div className='ticket-list-container'>
@@ -82,8 +34,8 @@ function TicketList({tickets,ticketId, updateCapacity}) {
             </div>
             
             <div className="buttons-container">
-              <button onClick={() => handleDelete(ticket.id)}>Update</button>
-              <button onClick={() => handleDelete(ticket.id)}>Delete</button>
+              {/* <button onClick={() => handleDelete(ticket.id)}>Update</button>
+              <button onClick={() => handleDelete(ticket.id)}>Delete</button> */}
               <button onClick={() => updateCapacity(ticket.id,ticket.available_tickets)}>
                 Buy button
               </button>
